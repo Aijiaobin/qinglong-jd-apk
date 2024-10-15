7f88410ad3bf809cc10c4bc3fa3950bc65128c46
Bing'home <wt08151217@gmail.com>
2024-10-15 20:06:22 +0800

    写定青龙地址

--- app/src/main/java/cn/moon/ql/data/model/QLSettingsData.java
+++ app/src/main/java/cn/moon/ql/data/model/QLSettingsData.java
@@ -27,6 +27,37 @@
 //         return csk;
 //     }
 // }
+// package cn.moon.ql.data.model;
+//
+// /**
+//  * Data class that captures user information for logged in users retrieved from LoginRepository
+//  */
+// public class QLSettingsData {
+//
+//     private String url;
+//     private String cid;
+//     private String csk;
+//
+//     // 硬编码的构造函数
+//     public QLSettingsData(String url, String cid,String csk) {
+//         this.url = "http://172.20.0.188:5700/";
+//         this.cid = "rgfzX7ft2O-H";
+//         this.csk = "a7Q_USD_M6yUz1Hc84uq_XxN";
+//     }
+//
+//     public String getUrl() {
+//         return url;
+//     }
+//
+//     public String getCid() {
+//         return cid;
+//     }
+//
+//     public String getCsk() {
+//         return csk;
+//     }
+// }
+
 package cn.moon.ql.data.model;
 
 /**
@@ -38,8 +69,7 @@ public class QLSettingsData {
     private String cid;
     private String csk;
 
-    // 硬编码的构造函数
-    public QLSettingsData() {
+    public QLSettingsData(String url, String cid,String csk) {
         this.url = "http://172.20.0.188:5700/";
         this.cid = "rgfzX7ft2O-H";
         this.csk = "a7Q_USD_M6yUz1Hc84uq_XxN";
